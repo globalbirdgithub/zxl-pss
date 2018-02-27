429c9098a5f447b18f533557928b68aca4ab3b7d
ZhangQiang <934017869@qq.com>
2018-02-27 13:53:55 +0800

    zxl-pss初始版本

---
+++ src/main/java/com/share/pss/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,12 @@
+package com.share.pss.service.impl;
+
+import com.share.pss.domain.Employee;
+import com.share.pss.service.IEmployeeService;
+
+/**
+ * @author MrZhang
+ * @date 2017年10月31日 下午12:31:44
+ * @version V1.0 业务层实现
+ */
+public class EmployeeServiceImpl extends BaseServiceImpl<Employee> implements IEmployeeService {
+}
